87cde5f99cf9f3c2599d15566619bf264a6e7a58
Felix <465332071@qq.com>
2021-11-22 16:32:19 +0800

    adBOMListMan created a machine&component screen

--- routes/admin.js
+++ routes/admin.js
@@ -14,7 +14,6 @@ var connection = mysql.createConnection({
     database : 'dbfordevelop', //开发用数据库
     multipleStatements: true,
     connectTimeout:false
-
     // host     : 'localhost',
     // port     : '3306',
     // user     : 'root',
