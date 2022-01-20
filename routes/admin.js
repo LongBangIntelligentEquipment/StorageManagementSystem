cc6c623584ff3592a10b210e3955a36d4af146da
LogiLuoZ <Christopherluo7@gmail.com>
2022-01-20 10:10:52 +0800

    upload FIle2

--- routes/admin.js
+++ routes/admin.js
@@ -31,6 +31,28 @@ var upload = multer({
 });
 //=========文件上传参数
 
+// -----测试upload文件夹分开放
+// var storage2 = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         // 接收到文件后输出的保存路径（若不存在则需要创建）
+//         cb(null, './public/components');
+//     },
+//     filename: function(req, file, cb) {
+//         var fileName='';
+//
+//         if(req.body.updateFileName!==undefined){
+//             fileName=file.originalname;
+//         }
+//         // 将保存文件名设置为 时间戳 + 文件原始名，比如 151342376785-123.jpg
+//         cb(null, file.originalname );
+//     }
+// });
+//
+// var upload2 = multer({
+//     storage: storage,
+// });
+
+
 function getInfo(url,callback){
     router.get('/create_qrcode', function (req, res, next) {
         var text = req.query.text;
